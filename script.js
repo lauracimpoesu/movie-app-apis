f3c01d37e695e79bb44813f370acd366868f5777
Laura Cimpoesu <lauracsbusiness@gmail.com>
2023-04-06 17:44:16 +0200

    optimization

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1"
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+  'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+const main = document.getElementById("main")
+const form = document.getElementById("form")
+const search = document.getElementById("search")
 
-// Get initial movies
-getMovies(API_URL);
+getMovies(API_URL)
 
 async function getMovies(url) {
-  const res = await fetch(url);
-  const data = await res.json();
+  const res = await fetch(url)
+  const data = await res.json()
 
-  showMovies(data.results);
+  showMovies(data.results)
 }
 
 function showMovies(movies) {
-  main.innerHTML = "";
+  main.innerHTML = ""
 
   movies.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
+    const { title, poster_path, vote_average, overview } = movie
 
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
+    const movieEl = document.createElement("div")
+    movieEl.classList.add("movie")
 
     movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
@@ -37,31 +36,31 @@ function showMovies(movies) {
           <h3>Overview</h3>
           ${overview}
         </div>
-        `;
-    main.appendChild(movieEl);
-  });
+        `
+    main.appendChild(movieEl)
+  })
 }
 
 function getClassByRate(vote) {
   if (vote >= 8) {
-    return "green";
+    return "green"
   } else if (vote >= 5) {
-    return "orange";
+    return "orange"
   } else {
-    return "red";
+    return "red"
   }
 }
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const searchTerm = search.value;
+  const searchTerm = search.value
 
   if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
+    getMovies(SEARCH_API + searchTerm)
 
-    search.value = "";
+    search.value = ""
   } else {
-    window.location.reload();
+    window.location.reload()
   }
-});
+})
